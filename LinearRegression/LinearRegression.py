1be0445ceff3f702da69e40558536a3c7b25e927
lechugalf <36242777+lechugalf@users.noreply.github.com>
2018-02-07 17:00:24 -0600

    Add files via upload

---
+++ LinearRegression/LinearRegression.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+class LinearRegression:
+    """Linear Regression by gradient descent"""
+
+    def __init__(self, tdata, name='unknown'):
+        self.name = name
+        self.tdata = tdata
+        self.param = [0, 0]
+        self.lrate = 0.001
+        self.N = len(tdata)
+        self.training_cost = []
+
+    def describeModel(self):
+        print "Model '{}'".format(self.name)
+        print "label x: " + ", ".join(map(str, self.tdata[:, 0]))
+        print "label y: " + ", ".join(map(str, self.tdata[:, 1]))
+        print "Parameters: {}, {}".format(self.param[0], self.param[1])
+        print "Cost: {}".format(self.costFunction())
+        print "Learning Rate: {}".format(self.lrate)
+
+    def modelFunction(self, xval):
+        return self.param[0] + (self.param[1] * xval)
+
+    def costFunction(self, x=None, y=None):
+        if (x != None) and (y != None):
+            self.param[0] = x
+            self.param[1] = y
+        cost = np.sum((self.modelFunction(self.tdata[:, 0]) - self.tdata[:, 1])
+                      **2)
+        return (cost / (2 * self.N))
+
+    def gradientDescent(self):
+        t1, t2 = 0, 0
+        t1 = np.sum(self.modelFunction(self.tdata[:, 0]) - self.tdata[:, 1])
+        t1 = self.lrate * (t1 / self.N)
+        t2 = np.sum((self.modelFunction(self.tdata[:, 0]) - self.tdata[:, 1]) *
+                    self.tdata[:, 0])
+        t2 = self.lrate * (t2 / self.N)
+        self.param[0] -= t1
+        self.param[1] -= t2
+
+    def training(self, times, lrate=None):
+        self.training_cost = []
+        if (lrate != None):
+            self.lrate = lrate
+        for step in range(0, times):
+            #result training cost
+            self.training_cost.append(self.costFunction())
+            #descent step
+            self.gradientDescent()
+            if (step % (times / 4) == 0):
+                print self.costFunction()
+        print 'training done...'
